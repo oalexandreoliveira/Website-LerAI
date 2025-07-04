65792bcb21457e207aba87acc693e4e59b1447c1
alexandre <oalexandreoliveira@hotmail.com>
2025-07-04 15:20:44 -0300

    alteração parcial

--- main.py
+++ main.py
@@ -96,4 +96,4 @@ def process_email():
         return jsonify({"error": f"Erro no processamento: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
